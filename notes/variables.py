57c5bf63e6e0d336d40aa89d4a5f789074847f2d
nicole12131 <neilybeth.salom@ucas-edu.net>
2025-08-22 09:53:18 -0600

    who_are_you.py

---
+++ notes/variables.py
@@ -0,0 +1,7 @@
+# NS 1st Variables notes 
+even_number = 2 
+grade = 99 
+animal = "Panda"
+print(grade)
+grade = 95 
+print(grade)
---
+++ notes/who_are_you.py
@@ -0,0 +1,6 @@
+# NS - CP1 Projects 
+name = input("What is your name:")
+age = input("How old are you: ")
+favoritecolor = input("What is your favorite color: ")
+
+print("Hello" ,name, "I am" ,age, "too!" , "I like", favoritecolor, "too")
