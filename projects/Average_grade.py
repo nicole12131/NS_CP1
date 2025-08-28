148a5aa14e6026e5fccf109a25ce0b120218932f
nicole12131 <neilybeth.salom@ucas-edu.net>
2025-08-28 09:54:29 -0600

    hi

---
+++ projects/Average_grade.py
@@ -0,0 +1,12 @@
+# NS 1st 🔨 Average Grade
+
+cs_grade = input("what is your grade in CS class? : ")
+math_grade = input("what is your grade in Math class? : ")
+biology_grade = input("what is your grade in Biology class? : ")
+advisory_grade = input("what is your grade in Advisory class? : ")
+drawing_grade = input("what is your grade in Drawing class? : ")
+english_grade = input("what is your grade in English class? : ")
+worldcivi_grade = input("what is your garde in Wolrd civilizations class? : ")
+
+calculation = (cs_grade + math_grade + biology_grade + advisory_grade / drawing_grade + english_grade + worldcivi_grade)
+total = round(calculation, 2)
