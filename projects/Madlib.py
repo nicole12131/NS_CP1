11082d223c6d6bbbf79a398bd7c7e914c44d6e2b
micole12131 <neilybeth.salom@ucas-edu.net>
2025-09-03 09:51:59 -0600

    Madlib project

---
+++ projects/Madlib.py
@@ -0,0 +1,19 @@
+# NS 1st 🔨 Madlib
+
+adjective_one = input("Tell me an adjective : ")
+adjective_two = input("Tell me another adjective : ")
+typeofbird = input("Tell me a type of bird : ")
+roominahouse= input("Tell me a room in a house : ")
+verb_pasttense = input("Tell me a verb past tense  ")
+verb = input("Tell me a verb : ")
+relative_name = input("Tell me a relative's name : ")
+noun = input("Tell me an noun : ")
+liquid = input("Tell me a type of liquid : ")
+verb_ining = input("Tell me a verb ending in-ing : ")
+partofthebody_plural = input("Tell me a part of the body plural ")
+plural_noun = input("Tell me a plural noun :  ")
+verbining_two = input("Tell me a verb ending in-ing : ")
+noun_two = input("Tell me an noun ")
+
+story = "It was a" + adjective_one, + "cold November day. I woke up to the" + adjective_two + "smell of" + typeofbird + "roasting in the" + roominahouse + "downstairs. I" + verb_pasttense + "down the stairs to see if I could help" + verb + "the dinner. My mom said 'See if" + relative_name + "needs a fersh" + noun + "' So I "
+
