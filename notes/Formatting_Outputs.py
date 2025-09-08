02102f2773fd2a2fa60140d51843b6c94674e996
micole12131 <neilybeth.salom@ucas-edu.net>
2025-09-08 09:34:57 -0600

    hi

---
+++ notes/Formatting_Outputs.py
@@ -0,0 +1,12 @@
+# NS 1st Formatting outputs notes
+
+# How do I write the format method?
+name = "Jake"
+age = 1200
+money = 25.1
+percent = .74
+
+print("Hello {}, you are {}. That is so old! You have ${: .2f} you most be rich! Random percent is {:%}. ".format(name, age, money, percent))
+
+
+print(f"Hello {name}, you are {age:,}. That is so old! You have ${money:.2f} you must be rich! Random percent is {percent:.1f}%.")
