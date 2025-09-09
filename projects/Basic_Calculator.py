3230bf40bbb1980a5cf9466ba772f8cf7f578948
micole12131 <neilybeth.salom@ucas-edu.net>
2025-09-09 09:52:19 -0600

    added ouput

--- projects/Basic_Calculator.py
+++ projects/Basic_Calculator.py
@@ -1,3 +1,14 @@
 # NS 1st 🔨 Basic Calculator 
 
-h
+num1 = 25
+num2 = 5
+
+
+print(f"{num1} + {num2} = 30  ")
+print(f"{num1} - {num2} = 20 ")
+print(f"{num1} * {num2} = 125 ")
+print(f"{num1} / {num2} = 5 ")
+print(f"{num1} // {num2} = ")
+print(f"{num1} % {num2} = ")
+print(f"{num1} ** {num2} = ")
+
