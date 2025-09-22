513c523b48c16a3f435a63b7fdee6266e9991d74
nicole12131 <neilybeth.salom@ucas-edu.net>
2025-09-22 22:39:55 +0000

    finish user singin

--- projects/user_singin.py
+++ projects/user_singin.py
@@ -1,25 +1,12 @@
 # NS 1st 🔨 User Sign In
 
-username = ("neily.salom")
-password = ("1234567")
-user_name = ("neilyx")
-pass_word = ("876543")
+correct_username = "neily.salom"
+correct_password = "1234567"
 
-username_one = input("Enter username: ")
-password_one = input("Enter password: ")
-username_two = input("Enter username: ")
-password_two = input("Enter password: ")
+username = input("Enter username: ")
+password = input("Enter password: ")
 
-
-if username_one == username:
-    if password_one == password:
-        print(f"Welcome {username_one}")
-    else:
-        print("Password invalid try again!")
-if username_two == user_name:
-    if password_two == pass_word:
-        print(f"Welcome {username_two}")
-    else:
-        print("Password invalid try again!")
+if username == correct_username and password == correct_password:
+    print("Welcome to the program,", username + "!")
 else:
-    print("Username invalid try again!")
+    print("Invalid login credentials. Please try again.")
