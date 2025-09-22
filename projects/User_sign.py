2dbe082cc3a8b1165c6d4ac761808f923ffcfcba
nicole12131 <neilybeth.salom@ucas-edu.net>
2025-09-22 09:53:38 -0600

    user

---
+++ projects/User_sign.py
@@ -0,0 +1,31 @@
+# NS 1st 🔨 User Sign In
+
+username = ("neily.salom")
+password = ("1234567")
+user_name = ("neilyx")
+pass_word = ("876543")
+
+username_one = input("Enter username: ")
+password_one = input("Enter password: ")
+username_two = input("Enter username: ")
+password_two = input("Enter password: ")
+
+
+if username_one == username :
+    print(f"username is correct")
+else:
+    print(f"Username invalid try again! ")
+if password_one == password: 
+        print(f"password is correct")
+        print(f"Welcome {username_one}")
+else: 
+        print(f"Password invalid try again!")
+    
+if username_two == user_name:
+      print(f"username is correct")
+else:
+    print(f"Username invalid try again! ")
+if password_two == pass_word:
+      print(f"password is correct")
+else: 
+        print(f"Password invalid try again!")
