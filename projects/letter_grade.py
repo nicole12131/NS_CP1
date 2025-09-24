cb50278432b385cb589fd56536a545556cc7d0bd
nicole12131 <neilybeth.salom@ucas-edu.net>
2025-09-24 23:44:28 +0000

    finish letter grade homework

--- projects/letter_grade.py
+++ projects/letter_grade.py
@@ -1,7 +1,30 @@
 # NS 1st 🔨 What is My Grade
 
-grade_percentage = float(input("Enter your grade percentage:  "))
+percentage = float(input("Enter your grade percentage:  "))
 
 
-if grade_percentage >= 100
+if percentage >= 93:
+    print(f"You have a {percentage}%, which is an A.")
+elif percentage >= 90:
+    print(f"You have a {percentage}%, which is an A-.")
+elif percentage >= 87:
+    print(f"You have a {percentage}%, which is a B+.")
+elif percentage >= 83:
+    print(f"You have a {percentage}%, which is a B.")
+elif percentage >= 80:
+    print(f"You have a {percentage}%, which is a B-.")
+elif percentage >= 77:
+    print(f"You have a {percentage}%, which is a C+.")
+elif percentage >= 73:
+    print(f"You have a {percentage}%, which is a C.")
+elif percentage >= 70:
+    print(f"You have a {percentage}%, which is a C-.")
+elif percentage >= 67:
+    print(f"You have a {percentage}%, which is a D+.")
+elif percentage >= 63:
+    print(f"You have a {percentage}%, which is a D.")
+elif percentage >= 60:
+    print(f"You have a {percentage}%, which is a D-.")
+else:
+    print(f"You have a {percentage}%, which is an F.")
  
