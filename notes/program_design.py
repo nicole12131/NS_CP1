7117b125825a9bf12ca2f825b36a8b58f4f56f1c
nicole12131 <neilybeth.salom@ucas-edu.net>
2025-09-25 09:52:51 -0600

    Debugging notes

---
+++ notes/program_design.py
@@ -0,0 +1,17 @@
+# NS 1st DEBUGGING NOTES!
+
+# Syntax Error - grammar error 
+# print"Vienna"
+# => Read error message, go to that line of code, fix the syntax
+
+# Logic Errors - We give the wrong steps
+numone = "2"
+numtwo = "5"
+
+print(numone + numtwo)
+
+ # Have well thought plan, Step by step go through our logic 
+
+ # Run-Time Error 
+
+ 
