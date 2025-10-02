f21184ecb877e956724f9b76c3d37762df2912a5
nicole12131 <neilybeth.salom@ucas-edu.net>
2025-10-02 23:58:45 +0000

    finish multipliaction table

--- projects/multiplication_table.py
+++ projects/multiplication_table.py
@@ -1,2 +1,15 @@
 # NS 1st 🔨 Multiplication
 
+print("     ", end="")
+for num in range(1, 13):
+    print(f"{num:4}", end="")
+print()
+print("-" * 53)
+
+
+for i in range(1, 13):
+    print(f"{i:3} |", end="")
+    for j in range(1, 13):
+        result = i * j
+        print(f"{result:4}", end="")
+    print()
