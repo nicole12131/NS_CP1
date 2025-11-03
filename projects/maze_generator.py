4a26f0e5316145536449df37a333adc2489fd340
nicole1213 <neilybeth.salom@ucas-edu.net>
2025-11-03 09:51:17 -0700

    maze generator

--- projects/maze_generator.py
+++ projects/maze_generator.py
@@ -1,5 +1,14 @@
 # Ns 1st 🔨 Maze Generator
-# import turtle
-import turtle as t
-# import random
+# import random as r
 import random as r 
+# import turtle as t
+import turtle 
+import time as t
+
+def setup_screen():
+    screen = turtle.Screen()
+    screen.screensize(6,6)
+    screen.title("Maze Generator")
+    screen.bgcolor("white")
+t1 = turtle.Turtle(shape="turtle")
+t1.forward(500)
