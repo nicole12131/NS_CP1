4b55be75d32f983661fff6bd89d9e246e6f0d728
nicole1213 <neilybeth.salom@ucas-edu.net>
2025-11-12 09:53:09 -0700

    maze generator

--- projects/order.py
+++ projects/order.py
@@ -1,11 +1,11 @@
 # NS 1st Order Up!
 #make the menu 
 menu = {
-    "Drinks": [],
+    "Drinks": ["Coca cola"],
     "Main Courses": [],
     "Side Dishes": [],
 }
-
+# User order 
 Your_order = {
     "Drink": "",
     "Main Course": "",
@@ -13,3 +13,12 @@ Your_order = {
     "Total cost": "",
 }
 
+# 
+drink = input("What ")
+main_course = input()
+side_dishes = input()
+
+if drink == menu:
+    Your_order["Drink"] = drink
+else: 
+    print()
