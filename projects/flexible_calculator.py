86cf96c87ee0335cbf2ce12432830b0bb322ada8
nicole12131 <neilybeth.salom@ucas-edu.net>
2025-11-18 09:52:32 -0700

    flexible calculator

--- projects/flexible_calculator.py
+++ projects/flexible_calculator.py
@@ -1,2 +1,16 @@
 # NS, 1st period, Flexible Calculator
+import time as t
+import random as r
+
+print("welcome to the Flexible Calculator!")
+print("Available operations: sum, average, max, min, product")
+
+available_operations = ["sum", "average", "max", "min", "product"]
+operation = input("which operation would you like to perfrom?: ")
+
+while True:
+    if operation == available_operations:
+        numbers = input("Enter numbers (type 'done' when finished): ")
+    else:
+        print(" That’s not a valid option, try again") 
 
