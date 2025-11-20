d2c524f5e7ff504dc9a6f5cbbdeeb3fa1db9e145
nicole12131 <neilybeth.salom@ucas-edu.net>
2025-11-20 09:52:40 -0700

    squares numbers

--- projects/squares.py
+++ projects/squares.py
@@ -3,8 +3,10 @@
 # make the list of numbers that you want to squared
 numbers = [3, 7, 12, 25, 30, 45, 50, 65, 70, 85, 90, 105, 110, 125, 130, 145, 150, 165, 170, 185, 190, 205, 210, 225, 230, 245, 250, 265, 270, 285]
 
-# calculate the squared of the numbers
-number = map(lambda num: num*num, numbers)
-for number in numbers:
-    squared = number*number
-    print(f"Original: {number}, Squared: {squared}")
+def multiply(num):
+    return num*num
+
+new_nums = map(lambda num: num*num, numbers)
+print(new_nums)
+for num in new_nums:
+    print(f"Original: {}, Squared: {num}")
