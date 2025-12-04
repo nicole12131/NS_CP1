8d7577e08cf889ba76e21dd45db1860c76c80403
nicole12131 <neilybeth.salom@ucas-edu.net>
2025-12-04 09:41:00 -0700

    finish pseudocode

--- projects/pseudocodee.py
+++ projects/pseudocodee.py
@@ -1,49 +1,129 @@
 # NS 1st Project Pseudocode
 
-# create a list that shows all of the locations of the game 
-
+# create a list that shows all of the locations of the game
+# The player can go to Dawn Winery, Starsnatch Cliff, Nameless Island, Anemo Archon Statue, Stormbearer Mountains, Springvale, Mondstadt City, Stormterror's Lair, and Starfell Lake.
 # define a function that allows the user to choose a location in the list
-    # ask user what location they want to explore
-    # if the location that the user choose is on the list it will return the location to the main game loop and it will continue the game
+	# ask user what location they want to explore
+# if the location that the user choose is on the list it will return the location to the main game loop and it will continue the game
+    # if the location is not in the list tell the user that the input is invalid and they need to try again
+
+
+# if the user choose to go to Mondstadt city
+# define function for Mondstadt city
+    # ask user if they want to stay or go to another location
+    # if user choose to stay
+    # user can buy things in the shops or they can explore
+    # if user choose to go to another location ask what location 
+
+# if the player choose to go to the Anemo Archon Statue
+# define function for Ameno Archon Statue
+    # ask user if they want to stay or go to another location
+    # if user choose to stay
+    # user can 
+    # if user choose to go to another location ask what location 
+    # if the location is not in the list tell the user that the input is invalid and they need to try again
+
+# if player choose to go to Dawn Winery
+# define function for Dawn Winery
+    # ask user if they want to stay or go to another location
+    # if user choose to stay
+    # user can fight with monsters that are close like a cryo slime or a Hilichurl 
+    # if user choose to go to another location ask what location 
+    # if the location is not in the list tell the user that the input is invalid and they need to try again
+
+# if player choose to go to Starsnatch Cliff
+# define function for Starsnatch Cliff
+    # ask user if they want to stay or go to another location
+    # if user choose to stay
+    # user can explore to find swords or flowers
+    # and they can fight with Hilichurl, slimes, Ruin Guard and eye of the Storm
+    # if user choose to go to another location ask what location 
+    # if the location is not in the list tell the user that the input is invalid and they need to try again
+
+# if player choose to go to Nameless Island 
+# define function for Nameless Island
+    # ask user if they want to stay or go to another location
+    # if user choose to stay
+    # user can explore 
+    # if user choose to go to another location ask what location 
+    # if the location is not in the list tell the user that the input is invalid and they need to try again
+
+# if player choose to go to Stormbearer Mountains
+# define function for Stormbearer Mountains
+    # ask user if they want to stay or go to another location
+    # if user choose to stay
+    # user can pick up dandelion Seed, and Valberry
+    # user can fight with Hilichurl, slimes, Ruin Guard, Cryo abyss mage, Anemo Hypostasis, samachurl, blazing Axe Mitachurl, and Fauti Electro cicin mage.
+    # if user choose to go to another location ask what location 
+    # if the location is not in the list tell the user that the input is invalid and they need to try again
+
+# if player choose to go to Springvale
+# define function for Springvale
+    # ask user if they want to stay or go to another location
+    # if user choose to stay
+    # user can explore 
+    # if user choose to go to another location ask what location 
+    # if the location is not in the list tell the user that the input is invalid and they need to try again
+
+# if player choose to go to Stormterror’s Lair
+# define function for Stormterror’s Lair
+    # ask user if they want to stay or go to another location
+    # if user choose to stay
+    # user can figth with Hilichurl, abyss mage,  Ruin Guard and Ruin Hunter
+    # user can fight with the final boss the dragon Stormterror
+    # if user choose to go to another location ask what location 
+    # if the location is not in the list tell the user that the input is invalid and they need to try again
+
+# if player choose to go to Starfell Lake
+# define function for Starfell Lake  
+    # ask user if they want to stay or go to another location
+    # if user choose to stay
+    # user can explore
+    # user can pick up fish 
+    # user can figth with hilichurl and samachurl
+    # if user choose to go to another location ask what location 
     # if the location is not in the list tell the user that the input is invalid and they need to try again
 
 # define a function for all of the locations so the user could explore them and pick up things from there
     # if the player is in that location for the first time they can pick up things
     # if the player is in that location for the second time they can't pick up things that they have in the inventory that means that they need to find things that they never pick up or they need to go to another location
-    # if the player pick up a new item the item is send to the inventory list
+    # if the player pick up a new item the item is sent to the inventory list
 
-# create a function that runs the combats 
-    # the user has four options punch, sword, use a potion or run away 
+
+# create a function that runs the combats
+    # the user has four options punch, sword, use a potion or run away
     # if the user choose to punch they make 1-3 of damage
     # if the user choose sword they make 3-6 of damage
-    # if the user choose potion they can see the inventory to choose a potion 
-    # if the user choose to run away they need to choose a location 
-    # inside of the function create a loop that repeats the combat 
-        # if the player has 0 of HP he dies and he need to restart the game
-        # if the abyss mage has 0 of HP the player wins and he can continue the game
-        # if the drangon has 0 of HP the player wins the dragon is now on their side and the player can continue the game
+    # if the user choose potion they can see the inventory to choose a potion
+    # if the user choose to run away they need to choose a location
+    # inside of the function create a loop that repeats the combat
+        # if the player has 0 of HP they dies and they need to restart the game
+        # if the abyss mage has 0 of HP the player wins, player can continue the game
+        # if the dragon has 0 of HP the player wins the dragon is now on their side, the player can continue the game
 
-# create a list that has all of the items that the player has pick up
+# create a list that has all of the items that the player has picked up
 
 #  define a function that shows the player's inventory
-    # the user has two options use an item or remove item from inventory 
+    # the user has two options use an item or remove item from inventory
     # if the player choose to use an item he can choose one of the items
     # if the items is not in the inventory the user need to try again
     # if the item is on the inventory they can use it and have the effect of that item
-    # if the player decides to remove an item 
+    # if the player decides to remove an item
     # if that item is not in the inventory the user needs to try again
-    # if the item is on the inventory the items is remove from the inventory 
+    # if the item is on the inventory the items is removed from the inventory
 
-# Create a loop that increases the player's stats 
-    # if the player level up all of the stats increases 
+
+# Create a loop that increases the player's stats
+    # if the player level up all of the stats increases
     # if the player complete a quest the magic, and defense increases
-    # if the player complete a quest the user gets new items to use 
-    # if the player gain experience the attack, HP and defense increases 
+    # if the player complete a quest the user gets new items to use
+    # if the player gain experience the attack, HP and defense increases
 
-# create a loop that triggers when the player dies 
-    # if user has 0 of HP the game will end 
-    # print a "You have died" message 
+
+# create a loop that triggers when the player dies
+    # if user has 0 of HP the game will end
+    # print a "You have died" message
     # ask user if they want to restart the game
-    # if user choose to restart the game will start again 
+    # if user choose to restart the game will start again
     # if the player decides not to restart the game ends
     # print "thanks for playing this game"
